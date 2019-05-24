75ce96d0e9ff6fb316654aa9f84dd7909fb5a72d
Tino Fuhrmann <tino-fuhrmann@web.de>
2019-05-24 22:33:29 +0200

    Added static gui

--- frontend/src/folder-overview.tsx
+++ frontend/src/folder-overview.tsx
@@ -4,7 +4,6 @@ export class FolderOverview extends React.Component<{}, {}> {
 
     render() { 
         return <div className="nav flex-column nav-pills folder-list" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-        
         <span className="align-middle"><a className="nav-link pl-3 active folder" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true"><i className="fas fa-inbox"></i> Inbox <span className="badge badge-pill badge-dark new-e-mail-counter">10</span></a></span>
         <a className="nav-link pl-4 folder" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Uni</a>
         <a className="nav-link pl-4 folder" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Work <span className="badge badge-pill badge-dark new-e-mail-counter">4</span></a>
