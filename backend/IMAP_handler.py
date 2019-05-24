b0569f9ea649d844e4c461f8e445aabb29bbc3b2
tweelix <felix.altenberg@outlook.com>
2019-05-24 23:56:01 +0200

    pep8 formatting

--- backend/IMAP_handler.py
+++ backend/IMAP_handler.py
@@ -2,27 +2,29 @@ import email
 import imaplib
 import mailparser
 
+
 class MailBox():
 
     def __init__(self, hostname, user, password, port=993):
-        self.MAX_MAIL=20
+        self.MAX_MAIL = 20
 
         self.connection = imaplib.IMAP4_SSL(hostname, port=port)
         self.connection.login(user, password)
-        
-        #create list of mailboxes
-        self.mailboxes=[]
+
+        # create list of mailboxes
+        self.mailboxes = []
         for i in self.connection.list()[1]:
             l = i.decode().split(' "/" ')
             if not "Calendar" in l[1] and not "Noselect" in l[0]:
                 self.mailboxes.append(l[1])
 
     def get_n_emails(self, mailbox):
-        _ , byteno = self.connection.select(mailbox, readonly=True)
+        _, byteno = self.connection.select(mailbox, readonly=True)
         return min(self.MAX_MAIL, int(byteno[0].decode()))
 
     def is_read(self, email_id):
-        unseen_list = self.connection.search(None, 'UNSEEN')[1][0].decode().split()
+        unseen_list = self.connection.search(
+            None, 'UNSEEN')[1][0].decode().split()
         if email_id in unseen_list:
             return False
         else:
@@ -31,24 +33,23 @@ class MailBox():
     def refresh_mailbox(self, mailbox):
         emails = []
         self.connection.select(mailbox, readonly=True)
-        #print(self.get_n_emails(mailbox))
+        # print(self.get_n_emails(mailbox))
         for i in range(1, self.get_n_emails(mailbox)):
-            _, msg_data=self.connection.fetch(str(i), "RFC822")
+            _, msg_data = self.connection.fetch(str(i), "RFC822")
             mail = mailparser.parse_from_bytes(msg_data[0][1])
             emails.append({"id": str(i),
-             "date": str(mail.date),
-             "delivered_to" : mail.delivered_to, 
-             "from": mail.from_,
-             "subject" : mail.subject, 
-             "to": mail.to, 
-             "timezone": mail.timezone,
-             "read": self.is_read(str(i))})
+                           "date": str(mail.date),
+                           "delivered_to": mail.delivered_to,
+                           "from": mail.from_,
+                           "subject": mail.subject,
+                           "to": mail.to,
+                           "timezone": mail.timezone,
+                           "read": self.is_read(str(i))})
         return emails
-
 
     def refresh_all(self):
         mail_preview_list = {}
-        #print(self.mailboxes)
+        # print(self.mailboxes)
 
         for m in self.mailboxes:
             mail_preview_list[m] = self.refresh_mailbox(m)
@@ -61,10 +62,9 @@ class MailBox():
 
     def get_mail(self, id, mailbox):
         self.connection.select(mailbox)
-        _, msg_data=self.connection.fetch(id, "RFC822")
+        _, msg_data = self.connection.fetch(id, "RFC822")
         mail = mailparser.parse_from_bytes(msg_data[0][1])
         return self.get_payload(mail)
 
     def __del__(self):
         self.connection.logout()
-
